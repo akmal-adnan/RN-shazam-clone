f866112d0656b20ea98005c14ff1f0fc29e45245
akmal-adnan <81947525+akmal-adnan@users.noreply.github.com>
2023-07-03 22:39:57 +0800

    float button changes

--- src/components/FloatButton.jsx
+++ src/components/FloatButton.jsx
@@ -1,32 +1,38 @@
 import React from 'react';
 import {StyleSheet, TouchableOpacity} from 'react-native';
 import Lottie from 'lottie-react-native';
+import Animated, {ZoomIn, ZoomOut} from 'react-native-reanimated';
 import {COLORS, LOTTIE} from '../constants';
 
-const FloatButton = ({navigation}) => (
-  <TouchableOpacity
-    onPress={() => navigation.push('MusicPlayer')}
-    activeOpacity={0.7}
-    style={{
-      position: 'absolute',
-      height: 57,
-      width: 57,
-      backgroundColor: COLORS.white1,
-      borderRadius: 80,
-      bottom: 26,
-      right: 16,
-      ...styles.shadow,
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}>
-    <Lottie
-      source={LOTTIE.SoundBar}
-      autoPlay
-      loop
-      style={{width: 35, height: 35}}
-    />
-  </TouchableOpacity>
-);
+const FloatButton = ({navigation}) => {
+  const AnimatedTouchable = Animated.createAnimatedComponent(TouchableOpacity);
+  return (
+    <AnimatedTouchable
+      entering={ZoomIn}
+      exiting={ZoomOut}
+      onPress={() => navigation.push('MusicPlayer')}
+      activeOpacity={0.7}
+      style={{
+        position: 'absolute',
+        height: 57,
+        width: 57,
+        backgroundColor: COLORS.white1,
+        borderRadius: 80,
+        bottom: 26,
+        right: 16,
+        ...styles.shadow,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Lottie
+        source={LOTTIE.SoundBar}
+        autoPlay
+        loop
+        style={{width: 35, height: 35}}
+      />
+    </AnimatedTouchable>
+  );
+};
 
 export default FloatButton;
 
